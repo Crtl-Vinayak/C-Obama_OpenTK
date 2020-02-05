38b8e0f24c9ce4bb473dce1efa4443bf2813e868
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-02-05 13:04:36 +0100

    changed project name to obomba_experimenting

---
+++ obomba_experimenting/obomba_experimenting/Shader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObamaV2 {
+    class Shader {
+
+        int Handle;
+
+        public Shader(string vertexPath, string fragmentPath) {
+            string VertexShaderSource;
+            string FragmentShaderSource;
+
+            using (StreamReader reader = new StreamReader(vertexPath, Encoding.UTF8)) {
+                VertexShaderSource = reader.ReadToEnd();
+            }
+
+            using (StreamReader reader = new StreamReader(fragmentPath, Encoding.UTF8)) {
+                FragmentShaderSource = reader.ReadToEnd();
+            }
+        }
+    }
+}
