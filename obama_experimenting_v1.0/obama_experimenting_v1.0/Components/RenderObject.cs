4b7776b53ddcad789e95682564828b709412f967
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-02-09 01:25:47 +0100

    Versie 1.2!!! Driehoek
    Overzichtelijke code hier (:

---
+++ obama_experimenting_v1.0/obama_experimenting_v1.0/Components/RenderObject.cs
@@ -0,0 +1,69 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace obama_experimenting_v1._0.Components
+{
+    class RenderObject : IDisposable
+    {
+        private bool _initialized;
+        private readonly int _vertexArray;
+        private readonly int _buffer;
+        private readonly int _verticeCount;
+        
+        public RenderObject(Vertex[] vertices)
+        {
+            _verticeCount = vertices.Length;
+            _vertexArray = GL.GenVertexArray();
+            _buffer = GL.GenBuffer();
+
+            GL.BindVertexArray(_vertexArray);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexArray);
+
+            GL.NamedBufferStorage(
+            _buffer,
+            Vertex.Size * vertices.Length,        // the size needed by this buffer
+            vertices,                           // data to initialize with
+            BufferStorageFlags.MapWriteBit);    // at this point we will only write to the buffer
+
+            GL.VertexArrayAttribBinding(_vertexArray, 0, 0);
+            GL.EnableVertexArrayAttrib(_vertexArray, 0);
+            GL.VertexArrayAttribFormat(_vertexArray, 0, 4, VertexAttribType.Float, false, 0);
+
+            GL.VertexArrayAttribBinding(_vertexArray, 1, 0);
+            GL.EnableVertexArrayAttrib(_vertexArray, 1);
+            GL.VertexArrayAttribFormat(_vertexArray, 1, 4, VertexAttribType.Float, false, 16);
+
+            GL.VertexArrayVertexBuffer(_vertexArray, 0, _buffer, IntPtr.Zero, Vertex.Size);
+            _initialized = true;
+
+        }
+
+        public void Render()
+        {
+            GL.BindVertexArray(_vertexArray);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (_initialized) {
+                    GL.DeleteVertexArray(_vertexArray);
+                    GL.DeleteBuffer(_buffer);
+                    _initialized = false;
+                }
+            }
+        }
+    }
+}
