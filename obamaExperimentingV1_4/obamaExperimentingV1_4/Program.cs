b9d0d47dac36f82202edc9849749ee942bd2590e
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-02-09 17:44:18 +0100

    v1_4 met een mooie ronddraaiende kubus

---
+++ obamaExperimentingV1_4/obamaExperimentingV1_4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenTK;
+
+namespace obamaExperimentingV1_4
+{
+    class ObamaOpenTK_CoreV1_4
+    {
+        static void Main(string[] args)
+        {
+            new DisplayStuff().Run(1.0 / 60.0);
+        }
+    }
+}
