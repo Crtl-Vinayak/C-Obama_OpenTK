cfc33b8e9d38d7d0c166fa7346d35aee862c5f37
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-02-09 14:29:30 +0100

    V1.3 = V1.2 met een wtf manier
    hier is de code om een domme driehoek te maken met een shitty manier

---
+++ obamaExperimentingV1_3/obamaExperimentingV1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenTK;
+
+namespace obamaExperimentingV1_3
+{
+    class ObamaOpenTK_CoreV1_3
+    {
+        static void Main(string[] args)
+        {
+            new DisplayStuff().Run(1.0 / 60.0);
+        }
+    }
+}
