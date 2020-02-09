5589e0af131ab32fe43950d302a86911463a86a1
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-02-09 23:33:15 +0100

    v1.8 incomplete (textures / shaders)

---
+++ obamaExperimentingV1_8/obamaExperimentingV1_8/DisplayStuff.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace obamaExperimentingV1_8
+{
+    class DisplayStuff : GameWindow
+    {
+
+        Dictionary<string, ShaderProgram> shaders = new Dictionary<string, ShaderProgram>();
+        string activeShader = "default";
+
+        Vector3[] vertdata;
+        Vector3[] coldata;
+
+        List<Volume> objects = new List<Volume>();
+
+        int[] indicedata;
+        int ibo_elements;
+        float time = 0.0f;
+
+        Camera cam = new Camera();
+        Vector2 lastMousePos = new Vector2();
+
+        bool isfullscreen = false;
+
+        public DisplayStuff() : base(1280, 720, new GraphicsMode(32, 24, 0, 4))
+        {
+            Title = "Obama Master OpenTK Yeeten";
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            //GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+            //Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 64.0f);
+            //GL.MatrixMode(MatrixMode.Projection);
+            //GL.LoadMatrix(ref projection);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            initProgram();
+
+            GL.ClearColor(Color.CornflowerBlue);
+            GL.PointSize(5f);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            ProcessInput();
+
+            List<Vector3> verts = new List<Vector3>();
+            List<int> inds = new List<int>();
+            List<Vector3> colors = new List<Vector3>();
+
+
+            int vertcount = 0;
+
+            foreach (Volume v in objects) {
+                verts.AddRange(v.GetVerts().ToList());
+                inds.AddRange(v.GetIndices(vertcount).ToList());
+                colors.AddRange(v.GetColorData().ToList());
+                vertcount += v.VertCount;
+            }
+
+            vertdata = verts.ToArray();
+            indicedata = inds.ToArray();
+            coldata = colors.ToArray();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, shaders[activeShader].GetBuffer("vPosition"));
+
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(vertdata.Length * Vector3.SizeInBytes), vertdata, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(shaders[activeShader].GetAttribute("vPosition"), 3, VertexAttribPointerType.Float, false, 0, 0);
+
+            if (shaders[activeShader].GetAttribute("vColor") != -1) {
+                GL.BindBuffer(BufferTarget.ArrayBuffer, shaders[activeShader].GetBuffer("vColor"));
+                GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(coldata.Length * Vector3.SizeInBytes), coldata, BufferUsageHint.StaticDraw);
+                GL.VertexAttribPointer(shaders[activeShader].GetAttribute("vColor"), 3, VertexAttribPointerType.Float, true, 0, 0);
+            }
+
+            objects[0].Position = new Vector3(0.3f, -0.5f + (float)Math.Sin(time), -3.0f);
+            objects[0].Rotation = new Vector3(0.55f * time, 0.25f * time, 0);
+            objects[0].Scale = new Vector3(0.1f, 0.1f, 0.1f);
+
+            objects[1].Position = new Vector3(-1f, 0.5f + (float)Math.Cos(time), -2.0f);
+            objects[1].Rotation = new Vector3(-0.25f * time, -0.35f * time, 0);
+            objects[1].Scale = new Vector3(0.25f, 0.25f, 0.25f);
+
+            objects[2].Position = new Vector3(1.8f, 0.1f + (float)Math.Tan(time), -5.0f);
+            objects[2].Rotation = new Vector3(-0.05f * time, -0.15f * time, 0);
+            objects[2].Scale = new Vector3(0.4f, 0.4f, 0.4f);
+
+            objects[3].Position = new Vector3(-4.0f, 1.0f + (float)Math.Cos(time), -4.0f);
+            objects[3].Rotation = new Vector3(-0.05f * time, -0.15f * time, 0);
+            objects[3].Scale = new Vector3(0.9f, 0.9f, 0.9f);
+
+            GL.UseProgram(shaders[activeShader].ProgramID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ibo_elements);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(indicedata.Length * sizeof(int)), indicedata, BufferUsageHint.StaticDraw);
+
+            foreach (Volume v in objects) {
+                v.CalculateModelMatrix();
+                //v.ViewProjectionMatrix = Matrix4.CreatePerspectiveFieldOfView(1.3f, ClientSize.Width / (float)ClientSize.Height, 1.0f, 40.0f);
+                v.ViewProjectionMatrix = cam.GetViewMatrix() * Matrix4.CreatePerspectiveFieldOfView(1.3f, ClientSize.Width / (float)ClientSize.Height, 1.0f, 40.0f);
+                v.ModelViewProjectionMatrix = v.ModelMatrix * v.ViewProjectionMatrix;
+            }
+            time += (float)e.Time;
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Viewport(0, 0, Width, Height);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Enable(EnableCap.DepthTest);
+
+            shaders[activeShader].EnableVertexAttribArrays();
+            //GL.EnableVertexAttribArray(attribute_vcol);
+
+            int indiceat = 0;
+
+            foreach (Volume v in objects) {
+                GL.UniformMatrix4(shaders[activeShader].GetUniform("modelview"), false, ref v.ModelViewProjectionMatrix);
+                GL.DrawElements(BeginMode.Triangles, v.IndiceCount, DrawElementsType.UnsignedInt, indiceat * sizeof(uint));
+                indiceat += v.IndiceCount;
+            }
+
+            shaders[activeShader].DisableVertexAttribArrays();
+            //GL.DisableVertexAttribArray(attribute_vcol);
+
+            GL.Flush();
+            SwapBuffers();
+        }
+
+        void initProgram()
+        {
+            lastMousePos = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+            CursorVisible = false;
+
+            GL.GenBuffers(1, out ibo_elements);
+
+            shaders.Add("default", new ShaderProgram("Shaders/vs.glsl", "Shaders/fs.glsl", true));
+
+            objects.Add(new Cube());
+            objects.Add(new Cube());
+            objects.Add(new Sierpinski());
+            objects.Add(new Sierpinski(4));
+        }
+
+        private void ProcessInput()
+        {
+
+            if (Keyboard.GetState().IsKeyDown(Key.W)) {
+                cam.Move(0f, 0.1f, 0f);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.S)) {
+                cam.Move(0f, -0.1f, 0f);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.A)) {
+                cam.Move(-0.1f, 0f, 0f);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.D)) {
+                cam.Move(0.1f, 0f, 0f);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.Space)) {
+                cam.Move(0f, 0f, 0.1f);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Key.ShiftLeft)) {
+                cam.Move(0f, 0f, -0.1f);
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.F11)) {
+                if (isfullscreen == true) {
+                    WindowState = WindowState.Normal;
+                    isfullscreen = false;
+                }
+                else {
+                    WindowState = WindowState.Fullscreen;
+                    isfullscreen = true;
+                }
+            }
+            if (Keyboard.GetState().IsKeyDown(Key.Escape)) {
+                Exit();
+            }
+
+            if (Focused) {
+                Vector2 delta = lastMousePos - new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+                lastMousePos += delta;
+
+                cam.AddRotation(delta.X, delta.Y);
+                lastMousePos = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+            }
+        }
+
+        protected override void OnFocusedChanged(EventArgs e)
+        {
+            base.OnFocusedChanged(e);
+            lastMousePos = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+        }
+    }
+}
---
+++ obamaExperimentingV1_8/obamaExperimentingV1_8/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenTK;
+
+namespace obamaExperimentingV1_8
+{
+    class ObamaOpenTK_CoreV1_8
+    {
+        static void Main(string[] args)
+        {
+            new DisplayStuff().Run(1.0 / 60.0);
+        }
+    }
+}
