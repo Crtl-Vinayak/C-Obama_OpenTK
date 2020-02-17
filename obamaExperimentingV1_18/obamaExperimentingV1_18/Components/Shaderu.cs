b9b589efaf174a36195549c88af4264f5a99c630
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-02-17 23:33:41 +0100

    v1.18 project made

---
+++ obamaExperimentingV1_18/obamaExperimentingV1_18/Components/Shaderu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace obamaExperimentingV1_18.Components
+{
+    public class Shaderu
+    {
+        public int CreateProgram()
+        {
+            try {
+                var program = GL.CreateProgram();
+                var shaders = new List<int>();
+                shaders.Add(CompileShader(ShaderType.VertexShader, @"Shaders/simplePipeVert.c"));
+                shaders.Add(CompileShader(ShaderType.FragmentShader, @"Shaders/simplePipeFrag.c"));
+
+                foreach (var shader in shaders) {
+                    GL.AttachShader(program, shader);
+                }
+
+                GL.LinkProgram(program);
+                var info = GL.GetProgramInfoLog(program);
+                if (!string.IsNullOrWhiteSpace(info)) {
+                    throw new Exception($"CompileShaders ProgramLinking had errors: {info}");
+                }
+
+                foreach (var shader in shaders) {
+                    GL.DetachShader(program, shader);
+                    GL.DeleteShader(shader);
+                }
+
+                return program;
+            } catch (Exception e) {
+                Debug.WriteLine(e);
+                throw;
+            }
+        }
+
+        public int CompileShader(ShaderType shaderType, string filepath)
+        {
+            var shader = GL.CreateShader(shaderType);
+            var src = File.ReadAllText(filepath);
+            GL.ShaderSource(shader, src);
+            GL.CompileShader(shader);
+            var info = GL.GetShaderInfoLog(shader);
+            if (!string.IsNullOrWhiteSpace(info))
+                throw new Exception($"CompileShader {shaderType} had errors: {info}");
+            return shader;
+        }
+    }
+}
---
+++ obamaExperimentingV1_18/obamaExperimentingV1_18/Components/ShapeObjectFactory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace obamaExperimentingV1_18.Components
+{
+    public class ShapeObjectFactory
+    {
+        public static Vertex[] CreateSolidCube(float side, Color4 color)
+        {
+            side /= 2f;
+
+            Vertex[] vertices =
+            {
+                new Vertex(new Vector4(-side, -side, -side, 1.0f),   color),
+                new Vertex(new Vector4(-side, -side, side, 1.0f),    color),
+                new Vertex(new Vector4(-side, side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(-side, side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(-side, -side, side, 1.0f),    color),
+                //new Vertex(new Vector4(-side, side, side, 1.0f),     color),
+                
+                //new Vertex(new Vector4(side, -side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(side, side, -side, 1.0f),     color),
+                //new Vertex(new Vector4(side, -side, side, 1.0f),     color),
+                //new Vertex(new Vector4(side, -side, side, 1.0f),     color),
+                //new Vertex(new Vector4(side, side, -side, 1.0f),     color),
+                //new Vertex(new Vector4(side, side, side, 1.0f),      color),
+                
+                //new Vertex(new Vector4(-side, -side, -side, 1.0f),   color),
+                //new Vertex(new Vector4(side, -side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(-side, -side, side, 1.0f),    color),
+                //new Vertex(new Vector4(-side, -side, side, 1.0f),    color),
+                //new Vertex(new Vector4(side, -side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(side, -side, side, 1.0f),     color),
+
+                //new Vertex(new Vector4(-side, side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(-side, side, side, 1.0f),     color),
+                //new Vertex(new Vector4(side, side, -side, 1.0f),     color),
+                //new Vertex(new Vector4(side, side, -side, 1.0f),     color),
+                //new Vertex(new Vector4(-side, side, side, 1.0f),     color),
+                //new Vertex(new Vector4(side, side, side, 1.0f),      color),
+
+                //new Vertex(new Vector4(-side, -side, -side, 1.0f),   color),
+                //new Vertex(new Vector4(-side, side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(side, -side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(side, -side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(-side, side, -side, 1.0f),    color),
+                //new Vertex(new Vector4(side, side, -side, 1.0f),     color),
+
+                //new Vertex(new Vector4(-side, -side, side, 1.0f),    color),
+                //new Vertex(new Vector4(side, -side, side, 1.0f),     color),
+                //new Vertex(new Vector4(-side, side, side, 1.0f),     color),
+                //new Vertex(new Vector4(-side, side, side, 1.0f),     color),
+                //new Vertex(new Vector4(side, -side, side, 1.0f),     color),
+                //new Vertex(new Vector4(side, side, side, 1.0f),      color),
+            };
+
+            return vertices;
+        }
+    }
+}
